35af039f424bc49d7fa59bb4084e786eede589a1
Mike Gordon <vprmatrix55@gmail.com>
2021-12-16 20:58:37 -0700

    add javascript code to run questions

--- assets/script.js
+++ assets/script.js
@@ -4,57 +4,87 @@ var questionText = document.getElementById("message");
 var infoText = document.getElementById("info");
 var startButton = document.getElementById("start");
 var answerBox = document.getElementById("choices");
+var result = document.getElementById("result");
 var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
-var resultText = document.getElementById("result");
 
 var questions = [
     {
-        "question": "insert question 1 here",
-        "option1": "give an answer choice",
-        "option2": "give an answer choice",
-        "option3": "give an answer choice",
-        "option4": "give an answer choice",
-
-        "answer": "give correct answer"
+        question: "insert question 1 here",
+        answers: [
+            { text: "give an answer choice", correct: false },
+            { text: "give an answer choice", correct: false },
+            { text: "give an answer choice", correct: false },
+            { text: "this is the right answer", correct: true },
+        ]
+    },
+   /* {
+        question: "insert question 2 here",
+        answers: {
+            a: "give an answer choice",
+            b: "give an answer choice",
+            c: "give an answer choice",
+            d: "give an answer choice"
+        },
+        correctAnswer: "c"
     },
     {
-        "question": "insert question 2 here",
-        "option1": "give an answer choice",
-        "option2": "give an answer choice",
-        "option3": "give an answer choice",
-        "option4": "give an answer choice",
-
-        "answer": "give correct answer"
+        question: "insert question 3 here",
+        answers: {
+            a: "give an answer choice",
+            b: "give an answer choice",
+            c: "give an answer choice",
+            d: "give an answer choice"
+        },
+        correctAnswer: "a"
     },
     {
-        "question": "insert question 3 here",
-        "option1": "give an answer choice",
-        "option2": "give an answer choice",
-        "option3": "give an answer choice",
-        "option4": "give an answer choice",
+        question: "insert question 4 here",
+        answers: {
+            a: "give an answer choice",
+            b: "give an answer choice",
+            c: "give an answer choice",
+            d: "give an answer choice"
+        },
+        correctAnswer: "b"
+    }*/
+];
 
-        "answer": "give correct answer"
-
-    },
-    {
-        "question": "insert question 4 here",
-        "option1": "give an answer choice",
-        "option2": "give an answer choice",
-        "option3": "give an answer choice",
-        "option4": "give an answer choice",
-
-        "answer": "give correct answer"
-
-    }
-]
 
 function swapThem() {
     answerBox.style.display = 'block';
     infoText.style.display = 'none';
     startButton.style.display = 'none';
+    questionText.innerHTML = questions[0].question /*this populates the first 
+    question, but I need to cycle through all of them with a pause, function rightWrong,
+    time reduction (if applicable), and end quiz after last question. */
+    questions.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add("button2")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerBox.appendChild(button)
+    })
+
 }
 
-console.log(highScore);
+function rightWrong() {
+    var selectedChoice;
+    if (questions[0].correctAnswer) {
+        result.innerHTML = "Correct";
+        result.style.display = 'block';
+    } else {
+        result.innerHTML = "Wrong";
+        result.style.display = 'block';
+    }
+};
+
+
+
+
+
