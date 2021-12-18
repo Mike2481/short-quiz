56a4c67e4edc0eec713bfebd7c5dbd85924cacc0
Mike Gordon <vprmatrix55@gmail.com>
2021-12-17 22:16:15 -0700

    add question sequencing and timer

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,135 @@
-var highScore = document.getElementById("high").innerHTML = "View High Score";
-var timer = document.getElementById("time").innerHTML = "Time Remaining";
+var highScore = document.getElementById("bestScore");
+var timer = document.getElementById("timeRemaining");
+
 var questionText = document.getElementById("message");
 var infoText = document.getElementById("info");
+
 var startButton = document.getElementById("start");
 var answerBox = document.getElementById("choices");
+var buttonA = document.getElementById("A");
+var buttonB = document.getElementById("B");
+var buttonC = document.getElementById("C");
+var buttonD = document.getElementById("D");
+
 var result = document.getElementById("result");
-var buttonA = document.getElementById("a");
-var buttonB = document.getElementById("b");
-var buttonC = document.getElementById("c");
-var buttonD = document.getElementById("d");
+
+
+
 
 var questions = [
     {
         question: "insert question 1 here",
-        answers: [
-            { text: "give an answer choice", correct: false },
-            { text: "give an answer choice", correct: false },
-            { text: "give an answer choice", correct: false },
-            { text: "this is the right answer", correct: true },
-        ]
+        answerA: "give an answer choice",
+        answerB: "give an answer choice",
+        answerC: "give an answer choice",
+        answerD: "correct answer",
+        correct: "D"
     },
-   /* {
+    {
         question: "insert question 2 here",
-        answers: {
-            a: "give an answer choice",
-            b: "give an answer choice",
-            c: "give an answer choice",
-            d: "give an answer choice"
-        },
-        correctAnswer: "c"
+        answerA: "give an answer choice",
+        answerB: "give an answer choice",
+        answerC: "correct answer",
+        answerD: "give an answer choice",
+        correct: "C"
     },
     {
         question: "insert question 3 here",
-        answers: {
-            a: "give an answer choice",
-            b: "give an answer choice",
-            c: "give an answer choice",
-            d: "give an answer choice"
-        },
-        correctAnswer: "a"
+        answerA: "correct answer",
+        answerB: "give an answer choice",
+        answerC: "give an answer choice",
+        answerD: "give an answer choice",
+        correct: "A"
     },
     {
         question: "insert question 4 here",
-        answers: {
-            a: "give an answer choice",
-            b: "give an answer choice",
-            c: "give an answer choice",
-            d: "give an answer choice"
-        },
-        correctAnswer: "b"
-    }*/
+        answerA: "give an answer choice",
+        answerB: "correct answer",
+        answerC: "give an answer choice",
+        answerD: "give an answer choice",
+        correct: "B"
+    }
 ];
+
+let questionIndex = questions.length - 1;
+let runningQuestionIndex = 0;
+
+function renderQuestion() {
+    let q = questions[runningQuestionIndex];
+    questionText.innerHTML = "<p>" + q.question + "</p>";
+    buttonA.innerHTML = q.answerA;
+    buttonB.innerHTML = q.answerB;
+    buttonC.innerHTML = q.answerC;
+    buttonD.innerHTML = q.answerD;
+
+}
+
+//let timer = 85;
+
+
+let score = 0;
+
+function checkAnswer(answer) {
+    if(answer == questions[runningQuestionIndex].correct) {
+        answerIsCorrect();
+    }else{
+        answerIsWrong();
+    }
+    if(runningQuestionIndex < questionIndex) {
+        runningQuestionIndex++;
+        setTimeout(renderQuestion(), 3000);
+    }else {
+        scoreRender();
+    }
+}
+
+function answerIsCorrect() {
+
+    result.style.display = 'block';
+
+    result.innerText = "Correct";
+}
+function answerIsWrong() {
+    result.style.display = 'block';
+    result.innerText = "Wrong";
+    // need 10 second off time
+}
+
+function time() {
+    var sec = 49;
+    var timerRun = setInterval(function(){
+        timer.innerHTML = '00: '+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timerRun);
+        }
+    }, 1000);
+    
+
+
+}
+
+/*function scoreRender() {
+    // need to convert time left to high score
+} */
 
 
 function swapThem() {
     answerBox.style.display = 'block';
     infoText.style.display = 'none';
     startButton.style.display = 'none';
-    questionText.innerHTML = questions[0].question /*this populates the first 
+    renderQuestion();
+    time();
+
+};
+
+
+
+
+    /*this populates the first 
     question, but I need to cycle through all of them with a pause, function rightWrong,
     time reduction (if applicable), and end quiz after last question. */
-    questions.answers.forEach(answer => {
+   /* questions.answers.forEach(answer => {
         var button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add("button2")
@@ -83,7 +152,7 @@ function rightWrong() {
         result.style.display = 'block';
     }
 };
-
+*/
 
 
 
